58bca6da00aad4e90d86e6cfff01a56e3ef5196e
HuKuTo3 <nvorlych@gmail.com>
2025-02-06 15:35:32 +0300

    commit

--- test.py
+++ test.py
@@ -1,7 +1,19 @@
 import cv2
 from ultralytics import YOLO
+from deepface import DeepFace
+import json
+import numpy as np
 
-model = YOLO("yolo11n.pt")
+def numpy_to_python(obj):
+    if isinstance(obj, np.integer):
+        return int(obj)
+    elif isinstance(obj, np.floating):
+        return float(obj)
+    elif isinstance(obj, np.ndarray):
+        return obj.tolist()
+    return obj
+
+model = YOLO("yolov8l-face.pt")
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
@@ -11,28 +23,32 @@ if not cap.isOpened():
 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fps = cap.get(cv2.CAP_PROP_FPS)
-out = cv2.VideoWriter("output_video_dark.avi", cv2.VideoWriter_fourcc(*'XVID'), fps, (frame_width, frame_height))
 
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
 
-    results = model(frame)
+    faces = model(frame)
 
-    for result in results[0].boxes:
-        x1, y1, x2, y2 = map(int, result.xyxy[0])
-        confidence = result.conf.item()
+    for face in faces[0].boxes:
+        x1, y1, x2, y2 = map(int, face.xyxy[0])
+        confidence = face.conf.item()
+        face_roi = frame[y1:y2, x1:x2]
+        result = DeepFace.analyze(face_roi, actions=['emotion'], enforce_detection=False)
+        # emotion = result[0]['dominant_emotion']
+        if result[0]:
+            serializable_result = json.loads(json.dumps(result[0], default=numpy_to_python))
+            with open('emotion_result.json', 'w') as json_file:
+                json.dump(serializable_result, json_file, indent=4)
 
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
         cv2.putText(frame, f"{confidence:.2f}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-    out.write(frame)
     cv2.imshow("YOLO Face Detection", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
-out.release()
 cv2.destroyAllWindows()
